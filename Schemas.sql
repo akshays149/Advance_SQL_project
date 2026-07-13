bc94cc8f6a592ecf2ee149b1a68464262c027fac
akshays149 <akshays2635@gmail.com>
2026-07-13 09:03:59 +0530

    Add files via upload

---
+++ Schemas.sql
@@ -0,0 +1,102 @@
+
+--creating a database for project by name 'datawarehouse'
+create database Data_warehouse
+
+--creating tables/schemas
+CREATE TABLE gold_customers(
+	customer_key int,
+	customer_id int,
+	customer_number varchar(50),
+	first_name varchar(50),
+	last_name varchar(50),
+	country varchar(50),
+	marital_status varchar(50),
+	gender varchar(50),
+	birthdate date,
+	create_date date
+);
+
+CREATE TABLE gold_products(
+	product_key int ,
+	product_id int ,
+	product_number nvarchar(50) ,
+	product_name nvarchar(50) ,
+	category_id nvarchar(50) ,
+	category nvarchar(50) ,
+	subcategory nvarchar(50) ,
+	maintenance nvarchar(50) ,
+	cost int,
+	product_line nvarchar(50),
+	start_date date 
+);
+
+CREATE TABLE gold_sales(
+	order_number nvarchar(50),
+	product_key int,
+	customer_key int,
+	order_date date,
+	shipping_date date,
+	due_date date,
+	sales_amount int,
+	quantity int,
+	price int 
+);
+
+--Importing data from wizard feature into the tables
+select * from gold_customers
+select * from gold_products
+select * from gold_sales
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
